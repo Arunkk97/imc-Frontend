911518af60362258f12a37276cded91d5803ab42
unknown <arunkk1997@gmail.com>
2024-05-21 17:55:20 +0530

    up

--- src/services/serverURL.js
+++ src/services/serverURL.js
@@ -1,2 +1,2 @@
-// export const SERVER_URL="https://imc-server.onrender.com"
-export const SERVER_URL='http://localhost:3000'
+export const SERVER_URL="https://imc-server.onrender.com"
+// export const SERVER_URL='http://localhost:3000'
